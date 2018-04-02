1f6f8cdc94a7512e453ef7c15801febadc02c090
esterTion <lcz970@126.com>
2018-04-02 16:54:28 +0800

    10000200

--- campaign_schedule.sql
+++ campaign_schedule.sql
@@ -1,3 +1,3 @@
 CREATE TABLE 'campaign_schedule' ('id' INTEGER NOT NULL, 'campaign_category' INTEGER NOT NULL, 'value' REAL NOT NULL, 'system_id' INTEGER NOT NULL, 'icon_image' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, PRIMARY KEY('id'))
-INSERT INTO `campaign_schedule` VALUES (/*id*/1, /*campaign_category*/34, /*value*/2000.0, /*system_id*/103, /*icon_image*/30, /*start_time*/"2019/02/28 05:00:00", /*end_time*/"2019/03/07 04:59:59");
-INSERT INTO `campaign_schedule` VALUES (/*id*/2, /*campaign_category*/44, /*value*/2000.0, /*system_id*/103, /*icon_image*/40, /*start_time*/"2019/02/28 05:00:00", /*end_time*/"2019/03/07 04:59:59");
+INSERT INTO `campaign_schedule` VALUES (/*id*/1, /*campaign_category*/34, /*value*/2000.0, /*system_id*/103, /*icon_image*/30, /*start_time*/"2018/03/06 05:00:00", /*end_time*/"2018/03/13 04:59:59");
+INSERT INTO `campaign_schedule` VALUES (/*id*/2, /*campaign_category*/44, /*value*/2000.0, /*system_id*/103, /*icon_image*/40, /*start_time*/"2018/03/06 05:00:00", /*end_time*/"2018/03/13 04:59:59");
