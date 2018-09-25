73cf3079c935bf230af4b1bd53fc8ef6d6cb83bd
esterTion-serveraccount <estertion@estertion.win>
2018-09-25 16:55:13 +0800

    10003750

--- gift_message.sql
+++ gift_message.sql
@@ -25,6 +25,7 @@ INSERT INTO `gift_message` VALUES (/*id*/9210, /*discription*/"LinksMateのス�
 INSERT INTO `gift_message` VALUES (/*id*/9211, /*discription*/"「DMM版事前登録キャンペーン」の10万人達成報酬です。", /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
 INSERT INTO `gift_message` VALUES (/*id*/9212, /*discription*/"ラジオ1周年記念生放送内コーナーの特別プレゼントです。", /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
 INSERT INTO `gift_message` VALUES (/*id*/9213, /*discription*/"運営からギルドハウスアイテムのプレゼントです。", /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
+INSERT INTO `gift_message` VALUES (/*id*/9214, /*discription*/"「公式生放送　2018　秋」内コーナーの特別プレゼントです。", /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
 INSERT INTO `gift_message` VALUES (/*id*/9301, /*discription*/"所持上限を超えて獲得したスタミナです。", /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
 INSERT INTO `gift_message` VALUES (/*id*/9302, /*discription*/"バトルアリーナの防衛報酬です。", /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
 INSERT INTO `gift_message` VALUES (/*id*/9303, /*discription*/"プリンセスアリーナの防衛報酬です。", /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
