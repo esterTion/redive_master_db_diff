d3ded65783e43739cdc8c3155212c5d124a65ebe
esterTion-serveraccount <estertion@estertion.win>
2018-11-20 18:25:49 +0800

    10004960

--- unit_unique_equip.sql
+++ unit_unique_equip.sql
@@ -1,1 +1,2 @@
 CREATE TABLE 'unit_unique_equip' ('unit_id' INTEGER NOT NULL, 'equip_slot' INTEGER NOT NULL, 'equip_id' INTEGER NOT NULL, PRIMARY KEY('unit_id'));
+INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105801, /*equip_slot*/1, /*equip_id*/130581);
