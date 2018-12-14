6d09edc59981273b99b9f52a376a24053264c790
esterTion-serveraccount <estertion@estertion.win>
2018-12-14 10:55:26 +0800

    10005500 - new event

--- tower_area_data.sql
+++ tower_area_data.sql
@@ -2,3 +2,4 @@ CREATE TABLE 'tower_area_data' ('tower_area_id' INTEGER NOT NULL, 'max_floor_num
 INSERT INTO `tower_area_data` VALUES (/*tower_area_id*/1, /*max_floor_num*/70, /*area_bg*/81000104, /*tower_bgm*/"bgm_M125");
 INSERT INTO `tower_area_data` VALUES (/*tower_area_id*/2, /*max_floor_num*/110, /*area_bg*/81000104, /*tower_bgm*/"bgm_M125");
 INSERT INTO `tower_area_data` VALUES (/*tower_area_id*/3, /*max_floor_num*/130, /*area_bg*/81000104, /*tower_bgm*/"bgm_M125");
+INSERT INTO `tower_area_data` VALUES (/*tower_area_id*/4, /*max_floor_num*/150, /*area_bg*/81000104, /*tower_bgm*/"bgm_M125");
