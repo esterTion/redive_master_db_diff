f5e84bbb4d2423ce7a5a1c95abde45e976fed6e3
esterTion-serveraccount <estertion@estertion.win>
2019-07-22 13:55:12 +0800

    10010550 - new clan battle

--- unit_unique_equip.sql
+++ unit_unique_equip.sql
@@ -11,6 +11,8 @@ INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/100901, /*equip_slot*/1, /*eq
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101001, /*equip_slot*/1, /*equip_id*/130101);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101101, /*equip_slot*/1, /*equip_id*/130111);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101201, /*equip_slot*/1, /*equip_id*/130121);
+INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101301, /*equip_slot*/1, /*equip_id*/130131);
+INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101401, /*equip_slot*/1, /*equip_id*/130141);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101501, /*equip_slot*/1, /*equip_id*/130151);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101601, /*equip_slot*/1, /*equip_id*/130161);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/101701, /*equip_slot*/1, /*equip_id*/130171);
@@ -46,6 +48,7 @@ INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105101, /*equip_slot*/1, /*eq
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105201, /*equip_slot*/1, /*equip_id*/130521);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105301, /*equip_slot*/1, /*equip_id*/130531);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105401, /*equip_slot*/1, /*equip_id*/130541);
+INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105601, /*equip_slot*/1, /*equip_id*/130561);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105701, /*equip_slot*/1, /*equip_id*/130571);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105801, /*equip_slot*/1, /*equip_id*/130581);
 INSERT INTO `unit_unique_equip` VALUES (/*unit_id*/105901, /*equip_slot*/1, /*equip_id*/130591);
