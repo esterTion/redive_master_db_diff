382ee6aa683ef1d878416a630272ac5836ff4057
esterTion-serveraccount <estertion@estertion.win>
2019-09-21 20:25:19 +0800

    10012100 - new clan battle

--- clan_cost_group.sql
+++ clan_cost_group.sql
@@ -2,6 +2,6 @@ CREATE TABLE 'clan_cost_group' ('id' INTEGER NOT NULL, 'cost_group_id' INTEGER N
 INSERT INTO `clan_cost_group` VALUES (/*id*/1, /*cost_group_id*/1, /*difficulty*/1, /*count*/1, /*cost*/300);
 INSERT INTO `clan_cost_group` VALUES (/*id*/2, /*cost_group_id*/1, /*difficulty*/1, /*count*/2, /*cost*/600);
 INSERT INTO `clan_cost_group` VALUES (/*id*/3, /*cost_group_id*/1, /*difficulty*/1, /*count*/3, /*cost*/900);
-INSERT INTO `clan_cost_group` VALUES (/*id*/4, /*cost_group_id*/2, /*difficulty*/1, /*count*/1, /*cost*/200);
-INSERT INTO `clan_cost_group` VALUES (/*id*/5, /*cost_group_id*/2, /*difficulty*/1, /*count*/2, /*cost*/400);
-INSERT INTO `clan_cost_group` VALUES (/*id*/6, /*cost_group_id*/2, /*difficulty*/1, /*count*/3, /*cost*/600);
+INSERT INTO `clan_cost_group` VALUES (/*id*/4, /*cost_group_id*/2, /*difficulty*/1, /*count*/1, /*cost*/300);
+INSERT INTO `clan_cost_group` VALUES (/*id*/5, /*cost_group_id*/2, /*difficulty*/1, /*count*/2, /*cost*/600);
+INSERT INTO `clan_cost_group` VALUES (/*id*/6, /*cost_group_id*/2, /*difficulty*/1, /*count*/3, /*cost*/900);
