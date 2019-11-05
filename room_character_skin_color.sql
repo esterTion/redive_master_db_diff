f850a3eea9c28661eb260af0ae0f6ae06160e2c6
esterTion-serveraccount <estertion@estertion.win>
2019-11-05 10:55:15 +0800

    10013100 - 2 new table: room_character_skin_color, room_skin_color

---
+++ room_character_skin_color.sql
@@ -0,0 +1,1 @@
+CREATE TABLE 'room_character_skin_color' ('character_id' INTEGER NOT NULL, 'skin_color_id' INTEGER NOT NULL, PRIMARY KEY('character_id'));
---
+++ room_skin_color.sql
@@ -0,0 +1,1 @@
+CREATE TABLE 'room_skin_color' ('skin_color_id' INTEGER NOT NULL, 'color_red' INTEGER NOT NULL, 'color_green' INTEGER NOT NULL, 'color_blue' INTEGER NOT NULL, PRIMARY KEY('skin_color_id'));
