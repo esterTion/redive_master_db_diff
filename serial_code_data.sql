47a0c2ec0515f1510c6fbe2ad2504b091fb09cc0
esterTion-serveraccount <estertion@estertion.win>
2020-06-08 18:25:23 +0800

    10018500 - 2 new table: serial_code_data, serial_group_data - new event スズナレインボー ステージ！

---
+++ serial_code_data.sql
@@ -0,0 +1,1 @@
+CREATE TABLE 'serial_code_data' ('serial_campaign_id' INTEGER NOT NULL, 'serial_group_id' INTEGER NOT NULL, 'campaign_name' TEXT NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, 'limit_num' INTEGER NOT NULL, PRIMARY KEY('serial_campaign_id'));
