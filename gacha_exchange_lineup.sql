cc3e208016d1d492d6eb201ff535c4014ff7a80d
esterTion-serveraccount <estertion@estertion.win>
2021-07-19 13:59:53 +0800

    10030200 - new gacha 限定キャラ復刻！出現率が期間中UP！\n『10回引く』で5等以上1個確定！

--- gacha_exchange_lineup.sql
+++ gacha_exchange_lineup.sql
@@ -225,4 +225,5 @@ INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/223, /*exchange_id*/142, /*uni
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/224, /*exchange_id*/143, /*unit_id*/107701, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/07/15 12:00:00", /*end_time*/"2021/07/19 14:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/225, /*exchange_id*/143, /*unit_id*/107901, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/07/15 12:00:00", /*end_time*/"2021/07/19 14:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/226, /*exchange_id*/144, /*unit_id*/117101, /*rarity*/3, /*gacha_bonus_id*/1052, /*start_time*/"2021/07/15 15:00:00", /*end_time*/"2021/07/24 14:59:59");
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/227, /*exchange_id*/145, /*unit_id*/110401, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/07/19 15:00:00", /*end_time*/"2021/07/24 14:59:59");
 CREATE INDEX 'gacha_exchange_lineup_0_exchange_id' on 'gacha_exchange_lineup'('exchange_id');
