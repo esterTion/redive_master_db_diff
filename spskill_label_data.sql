dd43920da8a2231e440761e7030e256fb9b97964
esterTion-serveraccount <estertion@estertion.win>
2021-10-19 16:30:18 +0800

    10032710

--- spskill_label_data.sql
+++ spskill_label_data.sql
@@ -1,3 +1,3 @@
 CREATE TABLE 'spskill_label_data' ('unit_id' INTEGER NOT NULL, 'normal_label_text' TEXT NOT NULL, 'sp_label_text' TEXT NOT NULL, PRIMARY KEY('unit_id'));
 INSERT INTO `spskill_label_data` VALUES (/*unit_id*/115801, /*normal_label_text*/"通常時", /*sp_label_text*/"変身中");
-INSERT INTO `spskill_label_data` VALUES (/*unit_id*/118501, /*normal_label_text*/"通常時", /*sp_label_text*/"RANK４以上時の特殊スキル");
+INSERT INTO `spskill_label_data` VALUES (/*unit_id*/118501, /*normal_label_text*/"通常時", /*sp_label_text*/"特殊スキル");
