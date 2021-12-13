65c2b9d69d154e81328400183b7e72b09dc41c3a
esterTion-serveraccount <estertion@estertion.win>
2021-12-13 11:00:51 +0800

    10034100 - new event デビュタント・シャングリ・ラ　聖夜のラブゲーム

--- dear_setting.sql
+++ dear_setting.sql
@@ -1,6 +1,5 @@
 CREATE TABLE 'dear_setting' ('event_id' INTEGER NOT NULL, 'system_name' TEXT NOT NULL, 'tutorial_quest_id' INTEGER NOT NULL, 'tutorial_chara_index' INTEGER NOT NULL, 'tutorial_story_id' INTEGER NOT NULL, PRIMARY KEY('event_id'));
 INSERT INTO `dear_setting` VALUES (/*event_id*/10053, /*system_name*/"信頼度", /*tutorial_quest_id*/10053102, /*tutorial_chara_index*/1, /*tutorial_story_id*/6017101);
-INSERT INTO `dear_setting` VALUES (/*event_id*/10054, /*system_name*/"信頼度", /*tutorial_quest_id*/10054103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6054101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10056, /*system_name*/"信頼度", /*tutorial_quest_id*/10056103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6056101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10060, /*system_name*/"信頼度", /*tutorial_quest_id*/10060103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6033201);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10061, /*system_name*/"信頼度", /*tutorial_quest_id*/10061106, /*tutorial_chara_index*/1, /*tutorial_story_id*/6061101);
@@ -15,6 +14,7 @@ INSERT INTO `dear_setting` VALUES (/*event_id*/10076, /*system_name*/"信頼度"
 INSERT INTO `dear_setting` VALUES (/*event_id*/10077, /*system_name*/"信頼度", /*tutorial_quest_id*/10077103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6050106);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10078, /*system_name*/"信頼度", /*tutorial_quest_id*/10078114, /*tutorial_chara_index*/1, /*tutorial_story_id*/6078101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10079, /*system_name*/"信頼度", /*tutorial_quest_id*/10079103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6052101);
+INSERT INTO `dear_setting` VALUES (/*event_id*/10081, /*system_name*/"信頼度", /*tutorial_quest_id*/10081103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6054101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/20015, /*system_name*/"信頼度", /*tutorial_quest_id*/0, /*tutorial_chara_index*/1, /*tutorial_story_id*/0);
 INSERT INTO `dear_setting` VALUES (/*event_id*/20017, /*system_name*/"信頼度", /*tutorial_quest_id*/0, /*tutorial_chara_index*/1, /*tutorial_story_id*/0);
 INSERT INTO `dear_setting` VALUES (/*event_id*/20018, /*system_name*/"信頼度", /*tutorial_quest_id*/0, /*tutorial_chara_index*/1, /*tutorial_story_id*/0);
