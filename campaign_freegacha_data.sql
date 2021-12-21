a058ea4d918cf9312ea52887ef246f551189d742
esterTion-serveraccount <estertion@estertion.win>
2021-12-21 17:30:56 +0800

    10034350

--- campaign_freegacha_data.sql
+++ campaign_freegacha_data.sql
@@ -71,3 +71,4 @@ INSERT INTO `campaign_freegacha_data` VALUES (/*id*/69, /*campaign_id*/21, /*gac
 INSERT INTO `campaign_freegacha_data` VALUES (/*id*/70, /*campaign_id*/21, /*gacha_id*/30164);
 INSERT INTO `campaign_freegacha_data` VALUES (/*id*/71, /*campaign_id*/21, /*gacha_id*/30166);
 INSERT INTO `campaign_freegacha_data` VALUES (/*id*/72, /*campaign_id*/21, /*gacha_id*/30167);
+INSERT INTO `campaign_freegacha_data` VALUES (/*id*/73, /*campaign_id*/22, /*gacha_id*/30170);
