3523712a4f328ed5d719ebd01f591db90259bfd2
esterTion-serveraccount <estertion@estertion.win>
2022-05-27 17:00:52 +0800

    10038800 - new unit: ★★★クリスティーナ - new gacha ★3キャラ出現率2倍！限定「ムイミ」「ネネカ」出現率UP！\n対象キャラを仲間にする度に対象メモピ200個GET！

--- wac_present_still_data.sql
+++ wac_present_still_data.sql
@@ -1,3 +1,4 @@
 CREATE TABLE 'wac_present_still_data' ('wac_id' INTEGER NOT NULL, 'date_id' INTEGER NOT NULL, 'still_id' INTEGER NOT NULL, PRIMARY KEY('wac_id','date_id'));
 INSERT INTO `wac_present_still_data` VALUES (/*wac_id*/2816, /*date_id*/421, /*still_id*/9000169);
 INSERT INTO `wac_present_still_data` VALUES (/*wac_id*/2816, /*date_id*/751, /*still_id*/9000170);
+INSERT INTO `wac_present_still_data` VALUES (/*wac_id*/2816, /*date_id*/1061, /*still_id*/9000210);
