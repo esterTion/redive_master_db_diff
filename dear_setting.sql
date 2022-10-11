2ffccf61944312397ed6811ccd085b44dc7a472d
esterTion-serveraccount <estertion@estertion.win>
2022-10-11 11:00:57 +0800

    10042100 - new event ハロウィンセイバーズ・スクランブル！　もふもふ捕獲大作戦 - new gacha 限定キャラ復刻！出現率が期間中UP！\n『10回引く』で5等以上1個確定！

--- dear_setting.sql
+++ dear_setting.sql
@@ -1,6 +1,5 @@
 CREATE TABLE 'dear_setting' ('event_id' INTEGER NOT NULL, 'system_name' TEXT NOT NULL, 'tutorial_quest_id' INTEGER NOT NULL, 'tutorial_chara_index' INTEGER NOT NULL, 'tutorial_story_id' INTEGER NOT NULL, PRIMARY KEY('event_id'));
 INSERT INTO `dear_setting` VALUES (/*event_id*/10053, /*system_name*/"信頼度", /*tutorial_quest_id*/10053102, /*tutorial_chara_index*/1, /*tutorial_story_id*/6017101);
-INSERT INTO `dear_setting` VALUES (/*event_id*/10076, /*system_name*/"信頼度", /*tutorial_quest_id*/10076105, /*tutorial_chara_index*/1, /*tutorial_story_id*/6076101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10078, /*system_name*/"信頼度", /*tutorial_quest_id*/10078114, /*tutorial_chara_index*/1, /*tutorial_story_id*/6078101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10083, /*system_name*/"信頼度", /*tutorial_quest_id*/10083103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6056101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10088, /*system_name*/"信頼度", /*tutorial_quest_id*/10088106, /*tutorial_chara_index*/1, /*tutorial_story_id*/6088201);
@@ -11,6 +10,7 @@ INSERT INTO `dear_setting` VALUES (/*event_id*/10094, /*system_name*/"信頼度"
 INSERT INTO `dear_setting` VALUES (/*event_id*/10095, /*system_name*/"信頼度", /*tutorial_quest_id*/10095103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6068101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10100, /*system_name*/"信頼度", /*tutorial_quest_id*/10100103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6100101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10102, /*system_name*/"信頼度", /*tutorial_quest_id*/10102106, /*tutorial_chara_index*/1, /*tutorial_story_id*/6102101);
+INSERT INTO `dear_setting` VALUES (/*event_id*/10103, /*system_name*/"信頼度", /*tutorial_quest_id*/10103105, /*tutorial_chara_index*/1, /*tutorial_story_id*/6076101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/20015, /*system_name*/"信頼度", /*tutorial_quest_id*/0, /*tutorial_chara_index*/1, /*tutorial_story_id*/0);
 INSERT INTO `dear_setting` VALUES (/*event_id*/20017, /*system_name*/"信頼度", /*tutorial_quest_id*/0, /*tutorial_chara_index*/1, /*tutorial_story_id*/0);
 INSERT INTO `dear_setting` VALUES (/*event_id*/20018, /*system_name*/"信頼度", /*tutorial_quest_id*/0, /*tutorial_chara_index*/1, /*tutorial_story_id*/0);
