856822d1bece54cc445f29c757d08bb8688f5591
esterTion-serveraccount <estertion@estertion.win>
2022-11-11 11:00:59 +0800

    10042900 - new event 魔法少女外伝　\nマジカル★ノワールズ！ - new gacha 限定キャラ復刻！出現率が期間中UP！\n『10回引く』で5等以上1個確定！

--- gacha_exchange_lineup.sql
+++ gacha_exchange_lineup.sql
@@ -431,4 +431,6 @@ INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/429, /*exchange_id*/226, /*uni
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/430, /*exchange_id*/226, /*unit_id*/113901, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2022/10/28 12:00:00", /*end_time*/"2022/10/31 11:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/431, /*exchange_id*/227, /*unit_id*/123801, /*rarity*/3, /*gacha_bonus_id*/1094, /*start_time*/"2022/10/31 12:00:00", /*end_time*/"2022/11/08 11:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/432, /*exchange_id*/228, /*unit_id*/121501, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2022/11/08 12:00:00", /*end_time*/"2022/11/11 11:59:59");
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/433, /*exchange_id*/229, /*unit_id*/107801, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2022/11/11 12:00:00", /*end_time*/"2022/11/15 14:59:59");
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/434, /*exchange_id*/229, /*unit_id*/107501, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2022/11/11 12:00:00", /*end_time*/"2022/11/15 14:59:59");
 CREATE INDEX 'gacha_exchange_lineup_0_exchange_id' on 'gacha_exchange_lineup'('exchange_id');
