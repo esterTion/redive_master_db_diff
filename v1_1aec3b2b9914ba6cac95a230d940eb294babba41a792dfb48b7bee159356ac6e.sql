d2e38928c8050dfee7968b2d1df4cee26c599e60
esterTion-serveraccount <estertion@estertion.win>
2024-06-05 11:01:40 +0800

    10056000

---
+++ v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e.sql
@@ -0,0 +1,11 @@
+CREATE TABLE 'v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e' ('a48246df0f8818532c9f8da15da3c493dbfd4832c9b72408404e1d9d80a00272' INTEGER NOT NULL, 'a467a8a91ca12fc63c31349260407f532ca636a3e395e9ce1fc50df7c76dd184' INTEGER NOT NULL, 'cec965da6ce8dcf0be4cc78bcc24259c774996d7e545da1d124f2181ddd84e84' TEXT NOT NULL, '2bf07945136c7e056ba1cd0b4c1db425b0a48f6df3b5999ea3a6b83621e4a0ba' TEXT NOT NULL, '70d1696f92980f0ad758fb78858ba674ac0421547db1b3da9e77cf83ebf35d92' INTEGER NOT NULL, '6b9c40c34cd8d021d6ede9d19b5fde3d748500a403c54faa47bfebc6aa176371' INTEGER NOT NULL, PRIMARY KEY('a48246df0f8818532c9f8da15da3c493dbfd4832c9b72408404e1d9d80a00272'));
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (101, 1, "特別講座プレゼンレポート", "ちぇるるんインパクト", 12, 13);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (102, 1, "特別講座プレゼンレポート", "クロエご明察", 13, 14);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (103, 1, "特別講座プレゼンレポート", "吹き散らせ先輩風", 14, 22);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (201, 2, "メルクリウス財団活動日誌", "アキノのリーダーシップ", 22, 23);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (202, 2, "メルクリウス財団活動日誌", "燃えろたい焼き魂", 23, 24);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (203, 2, "メルクリウス財団活動日誌", "麦しゅわに不可能なし", 24, 25);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (204, 2, "メルクリウス財団活動日誌", "ミフユの説教１日コース", 25, 32);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (301, 3, "ユニのメモ帳", "発泡と生菓子の哲学", 32, 33);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (302, 3, "ユニのメモ帳", "虎穴に潜らずんば", 33, 34);
+INSERT INTO `v1_1aec3b2b9914ba6cac95a230d940eb294babba41a792dfb48b7bee159356ac6e` VALUES (303, 3, "ユニのメモ帳", "動作試験と破滅の誘惑", 34, 0);
