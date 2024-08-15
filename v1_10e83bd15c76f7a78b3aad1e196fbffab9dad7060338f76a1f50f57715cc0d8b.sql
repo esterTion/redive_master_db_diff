4e7b086b102710879220ae757320a27e96697441
esterTion-serveraccount <estertion@estertion.win>
2024-08-15 14:01:37 +0800

    10057700

--- v1_10e83bd15c76f7a78b3aad1e196fbffab9dad7060338f76a1f50f57715cc0d8b.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE 'v1_10e83bd15c76f7a78b3aad1e196fbffab9dad7060338f76a1f50f57715cc0d8b' ('2aae20e0c64704b734c55f0fce3b4a00dd78aab27e50044ed263d554404fcdde' INTEGER NOT NULL, 'c85e59d06376bc02ff955fe1deb563ebde507d6eae5bb5ba2fe51f5af210d0d1' TEXT NOT NULL, PRIMARY KEY('2aae20e0c64704b734c55f0fce3b4a00dd78aab27e50044ed263d554404fcdde'));
-INSERT INTO `v1_10e83bd15c76f7a78b3aad1e196fbffab9dad7060338f76a1f50f57715cc0d8b` VALUES (1, 123456789);
-INSERT INTO `v1_10e83bd15c76f7a78b3aad1e196fbffab9dad7060338f76a1f50f57715cc0d8b` VALUES (2, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-INSERT INTO `v1_10e83bd15c76f7a78b3aad1e196fbffab9dad7060338f76a1f50f57715cc0d8b` VALUES (3, "abcdefghijklmnopqrstuvwxyz");
-INSERT INTO `v1_10e83bd15c76f7a78b3aad1e196fbffab9dad7060338f76a1f50f57715cc0d8b` VALUES (4, "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽぁぃぅぇぉゃゅょっ");
-INSERT INTO `v1_10e83bd15c76f7a78b3aad1e196fbffab9dad7060338f76a1f50f57715cc0d8b` VALUES (5, "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポァィゥェォャュョッ");
