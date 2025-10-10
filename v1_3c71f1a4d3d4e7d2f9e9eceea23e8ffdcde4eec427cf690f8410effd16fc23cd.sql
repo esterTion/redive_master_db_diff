49fa867930f032b442a8f1b3ae3a94dc2a5df29b
esterTion-serveraccount <estertion@estertion.win>
2025-10-10 11:07:34 +0800

    10065700

--- v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd.sql
+++ v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd.sql
@@ -1,7 +1,7 @@
-CREATE TABLE 'v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd' ('50788df9ee8da4f8d56681e352b2cfc08dd1686767e2d9423e90c1d4cfc423b6' TEXT NOT NULL, '63644bafdcbdffa9b5201144a7e73254cc7d1d0353c44f1b6cca90f2613c44a6' INTEGER NOT NULL, 'a351d681f77b6cae4007ddee453f30db83d132fd4e9b49e6f5b91c92a3d70d55' INTEGER NOT NULL, PRIMARY KEY('63644bafdcbdffa9b5201144a7e73254cc7d1d0353c44f1b6cca90f2613c44a6','a351d681f77b6cae4007ddee453f30db83d132fd4e9b49e6f5b91c92a3d70d55'));
-INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES ("獲得できる迷宮遺物がありませんでした。", 3, 0);
-INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES ("獲得できるコネクトサインがありませんでした。", 4, 0);
-INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES ("報酬を獲得できませんでした。", 8, 0);
-INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES ("アルファルピを獲得できませんでした。", 8, 1);
-INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES ("スロットを終了しました。", 8, 2);
+CREATE TABLE 'v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd' ('63644bafdcbdffa9b5201144a7e73254cc7d1d0353c44f1b6cca90f2613c44a6' INTEGER NOT NULL, 'a351d681f77b6cae4007ddee453f30db83d132fd4e9b49e6f5b91c92a3d70d55' INTEGER NOT NULL, '50788df9ee8da4f8d56681e352b2cfc08dd1686767e2d9423e90c1d4cfc423b6' TEXT NOT NULL, PRIMARY KEY('63644bafdcbdffa9b5201144a7e73254cc7d1d0353c44f1b6cca90f2613c44a6','a351d681f77b6cae4007ddee453f30db83d132fd4e9b49e6f5b91c92a3d70d55'));
+INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES (3, 0, "獲得できる迷宮遺物がありませんでした。");
+INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES (4, 0, "獲得できるコネクトサインがありませんでした。");
+INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES (8, 0, "報酬を獲得できませんでした。");
+INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES (8, 1, "アルファルピを獲得できませんでした。");
+INSERT INTO `v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd` VALUES (8, 2, "スロットを終了しました。");
 CREATE INDEX 'v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd_0_63644bafdcbdffa9b5201144a7e73254cc7d1d0353c44f1b6cca90f2613c44a6' on 'v1_3c71f1a4d3d4e7d2f9e9eceea23e8ffdcde4eec427cf690f8410effd16fc23cd'('63644bafdcbdffa9b5201144a7e73254cc7d1d0353c44f1b6cca90f2613c44a6');
